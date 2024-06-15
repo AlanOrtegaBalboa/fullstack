7914b10a0eff01c1769c57d86e160b41c00a17f7
AlanOrtegaBalboa <ortegabalboalan@hotmail.com>
2024-06-15 03:54:18 -0400

    backoffice middleware

--- app/Providers/BackofficeServiceProvider.php
+++ app/Providers/BackofficeServiceProvider.php
@@ -7,30 +7,17 @@ use Illuminate\Support\ServiceProvider;
 
 class BackofficeServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        Route::group(
-          [
-              'prefix' => 'backoffice',
-              'as' => 'backoffice.',
-              'namespace' => 'App\Http\Controllers\Backoffice',
-              'middleware' => [
-                  'backoffice'
-              ],
-
-          ],
-            fn ($router) => require __DIR__.'/../../routes/backoffice.php'
-        );
+        Route::middleware(['backoffice', 'auth', 'verified'])
+            ->prefix('backoffice')
+            ->as('backoffice.')
+            ->group(base_path('routes/backoffice.php')
+            );
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-
+        //
     }
 }
