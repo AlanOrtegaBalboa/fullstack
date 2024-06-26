d6a93fc58bdcc2b349f2958553189104f0b02f95
Alan Ortega <ortegabalboalan@hotmail.com>
2024-06-26 00:04:28 -0400

    Filtros con Patrón Pipeline
    INICIANDO LA LÓGICA DE FILTROS CON EL PATRÓN PIPELINE

---
+++ app/Filters/FilterValue.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Filters;
+
+
+class FilterValue
+{
+    public function __construct(private readonly array|string|bool|null $value)
+    {}
+
+    public function getValue(): array|string|bool|null
+    {
+        return $this->value;
+    }
+}
