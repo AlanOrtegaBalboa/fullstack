be421f88c1b791528c81e9db82eb36943c07917b
Alan Ortega <ortegabalboalan@hotmail.com>
2024-07-02 02:41:28 -0400

    Desarrollando las acciones de las categorias

---
+++ app/Actions/Categories/CreateCategoryAction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Actions\Categories;
+
+use App\Actions\Contracts\CreateAction;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+
+final class CreateCategoryAction implements CreateAction
+{
+    public static function execute(array $data = []): Category
+    {
+        return Category::create([
+            'name' => data_get($data, 'name'),
+            'description' => data_get($data, 'description'),
+            'is_active' => data_get($data, 'is_active', false),
+            'created_at' => data_get($data, 'created_at', now()),
+        ]);
+    }
+}
---
+++ app/Actions/Contracts/UpdateAction.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Actions\Contracts;
+
+
+interface UpdateAction extends Action
+{
+    public static function execute(int $id, array $data = []): void;
+}
