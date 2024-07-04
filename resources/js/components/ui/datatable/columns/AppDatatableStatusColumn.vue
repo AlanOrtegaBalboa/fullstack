b925854f5dc29979677069790ba7ac45b3f9eb93
Alan Ortega <ortegabalboalan@hotmail.com>
2024-07-04 14:24:50 -0400

    componentes columna del datatable
    DESARROLLANDO LOS COMPONENTES COLUMNA CON VUE DE NUESTRO DATATABLE

---
+++ resources/js/components/ui/datatable/columns/AppDatatableStatusColumn.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: "AppDataTableStatusColumn",
+}
+</script>
+
+<script setup>
+defineProps({
+    value: {
+        type: String,
+    },
+    trueValue: {
+        type: String,
+        default: 'Online',
+    },
+    falseValue: {
+        type: String,
+        default: 'Offline',
+    },
+})
+</script>
+
+<template>
+    <td class="text-sm font-medium text-white px-6 py-4">
+        <span
+            :class="{
+                'bg-green-200 text-green-800': value === trueValue,
+                'bg-red-200 text-red-800': value === trueValue,
+            }"
+            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full"
+        >
+            {{ value }}
+        </span>
+    </td>
+</template>
+
---
+++ resources/js/components/ui/datatable/columns/index.js
@@ -0,0 +1,19 @@
+import AppDatatableActionsColumn from './AppDatatableActionsColumn.vue';
+import AppDatatableBadgeColumn from "./AppDatatableBadgeColumn.vue";
+import AppDatatableBooleanColumn from "./AppDatatableBooleanColumn.vue";
+import AppDatatableDateColumn from "./AppDatatableDateColumn.vue";
+import AppDatatableEmailColumn from "./AppDatatableEmailColumn.vue";
+import AppDatatableImageColumn from "./AppDatatableImageColumn.vue";
+import AppDatatableStatusColumn from "./AppDatatableStatusColumn.vue";
+import AppDatatableTextColumn from "./AppDatatableTextColumn.vue";
+
+export {
+    AppDatatableActionsColumn,
+    AppDatatableBadgeColumn,
+    AppDatatableBooleanColumn,
+    AppDatatableDateColumn,
+    AppDatatableEmailColumn,
+    AppDatatableImageColumn,
+    AppDatatableStatusColumn,
+    AppDatatableTextColumn,
+};
