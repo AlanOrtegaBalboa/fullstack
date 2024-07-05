0043032d0b09dee40050d8669c6f1249ff66f6f4
Alan Ortega <ortegabalboalan@hotmail.com>
2024-07-05 01:22:57 -0400

    Paginación del datatable
    DESARROLLANDO LA PAGINACIÓN CON VUE DE NUESTRO DATATABLE

---
+++ resources/js/components/ui/datatable/AppDatatablePaginationCurrent.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "AppDataTablePaginationCurrent",
+}
+</script>
+
+<template>
+    <a
+        class="flex items-center justify-center px-3 h-8 text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+        href="#"
+    >
+        <slot/>
+    </a>
+</template>
---
+++ resources/js/components/ui/datatable/index.js
@@ -0,0 +1,7 @@
+import AppDatatablePaginationLinks from "./AppDatatablePaginationLinks.vue";
+import AppDatatablePaginationSummary from "./AppDatatablePaginationSummary.vue";
+
+export default {
+    AppDatatablePaginationLinks,
+    AppDatatablePaginationSummary,
+};
